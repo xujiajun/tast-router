145c3ba42e9b2b6a5613cbf4090d353e538f0b4e
xujiajun <xujiajun@superu.org>
2015-07-16 15:12:06 +0800

    add tests

--- tests/src/TastRouter/RouterTest.php
+++ tests/src/TastRouter/RouterTest.php
@@ -37,10 +37,8 @@ class RouterTest extends PHPUnit_Framework_TestCase
 
     public function testxactlyMatch()
     {
-        $collection = new RouteCollection();
-        list($route1, $route2, $route3) = $this->getRoutes();
-        $collection->attachRoute($route3);
-        $router = new Router($collection);
+        list($route1, $route2, $route3, $route4) = $this->getRoutes();
+        $router = $this->_getRouterByRoute($route3);
         $router->match('/foo1', 'GET');
     }
 
@@ -82,17 +80,41 @@ class RouterTest extends PHPUnit_Framework_TestCase
 
     public function testMethod2()
     {
-        $collection = new RouteCollection();
-        list($route1, $route2, $route3) = $this->getRoutes();
-        $collection->attachRoute($route3);
-        $router = new Router($collection);
+        list($route1, $route2, $route3, $route4) = $this->getRoutes();
+        $router = $this->_getRouterByRoute($route3);
         $_SERVER['REQUEST_URI'] = '/foo1?name=xx';
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $result = $router->matchCurrentRequest();
 
 //        var_dump($result);
-        $this->assertEquals($route3->getUrl(),'/foo1');
+        $this->assertEquals($route3->getUrl(), '/foo1');
         $this->assertNull($result);
+    }
+
+    /**
+     * @expectedException     Exception
+     */
+    public function testWhenUrlPartHasRouteNameKey()
+    {
+        list($route1, $route2, $route3, $route4) = $this->getRoutes();
+        $router = $this->_getRouterByRoute($route4);
+        $routeNameKey = $route4->getNameKey();
+        $router->match("/hello/$routeNameKey", 'GET');
+    }
+
+    public function testWhenMissRequireParam()
+    {
+        list($route1, $route2, $route3, $route4) = $this->getRoutes();
+        $router = $this->_getRouterByRoute($route4);
+        $routeNameKey = $route4->getNameKey();
+        $router->match("/hello/xujiajun", 'GET');
+    }
+
+    private function _getRouterByRoute($route)
+    {
+        $collection = new RouteCollection();
+        $collection->attachRoute($route);
+        return  new Router($collection);
     }
 
     private function getRoutes()
@@ -114,7 +136,13 @@ class RouterTest extends PHPUnit_Framework_TestCase
             '_controller' => 'TastRouter\\Test\\controllers\\FooController::indexAction',
             'methods' => 'GET'
         ]);
-        return [$route1, $route2, $route3];
+
+        $route4 = new Route('/hello/{name}', [
+            '_controller' => 'TastRouter\\Test\\controllers\\FooController::indexAction',
+            'methods' => 'GET',
+        ]);
+
+        return [$route1, $route2, $route3, $route4];
     }
 
 }
